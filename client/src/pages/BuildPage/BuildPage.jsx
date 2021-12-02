948496569a030aff568feeaa6aec78c3b5960f92
Eddie <ed201112@gmail.com>
2021-12-01 23:12:54 -0800

    frontend pages

---
+++ client/src/pages/BuildPage/BuildPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BuildPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
