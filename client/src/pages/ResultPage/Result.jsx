bd1ce1042a4d9a5b8a17d4c7b323356ba9ce61b9
Eddie <ed201112@gmail.com>
2021-12-08 10:45:21 -0800

    result page fin

--- client/src/pages/ResultPage/Result.jsx
+++ client/src/pages/ResultPage/Result.jsx
@@ -1,11 +1,14 @@
 import axios from 'axios';
 import React, { useState, useEffect }  from 'react'
 import { useParams } from 'react-router-dom'
+import "./Result.scss"
 
 export default function Result() {
     const params = useParams();
     const [prices, setPrices] = useState(null);
     const [model, setModel] = useState(null);
+    const [salary, setSalary] = useState(null);
+
     useEffect(() => {
         axios.get("http://localhost:8080/prices")
         .then(response => {
@@ -14,11 +17,14 @@ export default function Result() {
         }).then(axios.get(`http://localhost:8080/model/${params.userId}/${params.modelId}`)
         .then(response => {
             setModel(response.data);
-        }));
-    }, [])
+        }).then(axios.get(`http://localhost:8080/user/${params.userId}`)
+        .then(response => {
+            setSalary(response.data.salary);
+        })));
+    }, [params.userId, params.modelId])
 
 
-    if (!model || !prices) {
+    if (!model || !prices || !salary) {
         return (
             <div>
                 
@@ -27,6 +33,7 @@ export default function Result() {
     }
     console.log(model);
     console.log(prices);
+    console.log(salary);
     let total = 0;
     total += parseInt(prices[`${model.style}_${model.land}`]);
     if (model.location === "vancouver"){
@@ -39,10 +46,21 @@ export default function Result() {
     } else if (model.age > 20) {
         total = total * 0.8;
     }
-    console.log(total);
+    const month = (Math.log(1+(total*0.95*0.0025)/(salary/12/3))/Math.log(1+0.0025));
+
     return (
-        <div>
-            
+        <div className="result">
+            <div className="result__title">Your home price is estimated at ${total}</div>
+            <div className="result__detail">At a interest rate of 3% a year, 
+            with your yearly take home salary of ${salary} and 5% down payment, 
+            it is recommended that you take out a {month/12} years loan.</div>
+
+            <ul className="result__detail">
+                <li className="result__item">Model Id: {model.id}</li>
+                <li className="result__item">Location: {model.location}</li>
+                <li className="result__item">Type: {model.style}</li>
+                <li className="result__item">Land size: {model.land}</li>
+            </ul>
         </div>
     )
 }
