0ef75cf45e426671b62e52e9cfe4e893f1faf483
Eddie <ed201112@gmail.com>
2022-01-06 23:51:43 -0800

    login signup style

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,16 +1,27 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import "./SignUp"
+import "./SignUp.scss"
 
 
 export default function SignUp(props) {
     // pop up from login page
     const [errMessage, seterrMessage] = useState("");
+
+    const checkPass = (e) => {
+        e.preventDefault();
+        console.log(e);
+        if (e.target.form[1].value !== e.target.value) {
+            seterrMessage("Passwords don't match.");
+        } else {
+            seterrMessage("");
+        }
+    };
+
     const signUp = (e) => {
         e.preventDefault();
         axios.post("http://localhost:8080/user/signup", {
-            email: e.target.email.value,
-            password: e.taget.password.value,
+            email: e.target["email_sign"].value,
+            password: e.taget["password_sign"].value,
             salary: e.target.salary.value
         }).then(response => {
             if (!response.data.message) {
@@ -25,50 +36,58 @@ export default function SignUp(props) {
     };
 
     return (
-        <div className='signup' style={{display:props.display}}>
-            <form action="" className='signup__form' method="post" id="signup__form" onSubmit={(event) => signUp(event)}>
-            <label className="signup__label" htmlFor="email">Email</label>
-            <input
-              className="signup__field"
-              type="text"
-              name="email"
-              id="email"
-              placeholder="example@email.com"
-              required
-              pattern=""
-            ></input>
-            <label className="signup__label" htmlFor="password">Password</label>
-            <input
-              className="signup__field"
-              type="text"
-              name="password"
-              id="password"
-              required
-              pattern=""
-            ></input>
-            <label className="signup__label" htmlFor="password__re">Re-enter Password</label>
-            <input
-              className="signup__field"
-              type="text"
-              name="password__re"
-              id="password__re"
-              required
-              pattern=""
-            ></input>
-            <label className="signup__label" htmlFor="salary">What is your monthly salary?</label>
-            <input
-              className="signup__field"
-              type="text"
-              name="salary"
-              id="salary"
-              placeholder="monthly salary..."
-              required
-              pattern="^[0-9]+$"
-            ></input>
-            </form>
-            <button className="signup__submit" type="submit" form="signup__form" >Sign Up</button>
-            <button className="signup__cancel" type="click" onClick={props.cancel}  >Cancel</button>
-            <div className='signup__message'>{errMessage}</div>
+        <div className="signup__background" style={{display:props.display}}>
+            <div className='signup'>
+                <form action="" className='signup__form' method="post" id="signup__form" onSubmit={(event) => signUp(event)}>
+                {/* <label className="signup__label" htmlFor="email">Email</label> */}
+                <input
+                  className="signup__field"
+                  type="text"
+                  name="email_sign"
+                  id="email_sign"
+                  placeholder="Email"
+                  required
+                ></input>
+                {/* <label className="signup__label" htmlFor="password">Password</label> */}
+                <input
+                  className="signup__field"
+                  type="password"
+                  name="password_sign"
+                  id="password_sign"
+                  placeholder="Password"
+                  minLength="8"
+                  required
+                  pattern=""
+                ></input>
+                {/* <label className="signup__label" htmlFor="password__re">Re-enter Password</label> */}
+                <input
+                  className="signup__field"
+                  type="password"
+                  name="password__re"
+                  id="password__re"
+                  placeholder="Retype password"
+                  minLength="8"
+                  required
+                  pattern=""
+                  onChange={checkPass}
+                ></input>
+                {/* <label className="signup__label" htmlFor="salary">What is your monthly salary?</label> */}
+                <input
+                  className="signup__field"
+                  type="text"
+                  name="salary"
+                  id="salary"
+                  placeholder="Enter monthly salary"
+                  required
+                  pattern="^[0-9]+$"
+                ></input>
+                </form>
+                <div className="signup__button">
+                    <button className="signup__submit" type="submit" form="signup__form" >Sign Up</button>
+                    <button className="signup__cancel" type="click" onClick={props.cancel}  >Cancel</button>
+                </div>
+                <div className='signup__message'>{errMessage}</div>
+            </div>
         </div>
     )
 }
